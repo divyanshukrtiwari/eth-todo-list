8d15371454be85ba93c3d142a2c64b802def6225
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-07-28 13:53:00 +0530

    updated smart conteact

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.5.12 <0.7.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("Check out https://github.com/divyanshukrtiwari");
+    }
+
+    mapping(uint=> Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
